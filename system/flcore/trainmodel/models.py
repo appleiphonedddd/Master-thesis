9d3bed87d550e61452f6e2a9d1b12facd607072d
Egor Alekseyevich Morozov <zozo5120@outlook.com>
2025-09-12 13:11:25 +0800

    2025-09-12

--- system/flcore/trainmodel/models.py
+++ system/flcore/trainmodel/models.py
@@ -4,7 +4,6 @@ from torch import nn
 
 batch_size = 10
 
-
 # split an original model into a base and a head
 class BaseHeadSplit(nn.Module):
     def __init__(self, base, head):
@@ -109,39 +108,8 @@ class AmazonMLP(nn.Module):
         out = self.encoder(x)
         out = self.fc(out)
         return out
-        
 
-# # https://github.com/katsura-jp/fedavg.pytorch/blob/master/src/models/cnn.py
-# class FedAvgCNN(nn.Module):
-#     def __init__(self, in_features=1, num_classes=10, dim=1024):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(in_features,
-#                                32,
-#                                kernel_size=5,
-#                                padding=0,
-#                                stride=1,
-#                                bias=True)
-#         self.conv2 = nn.Conv2d(32,
-#                                64,
-#                                kernel_size=5,
-#                                padding=0,
-#                                stride=1,
-#                                bias=True)
-#         self.fc1 = nn.Linear(dim, 512)
-#         self.fc = nn.Linear(512, num_classes)
-
-#         self.act = nn.ReLU(inplace=True)
-#         self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
-
-#     def forward(self, x):
-#         x = self.act(self.conv1(x))
-#         x = self.maxpool(x)
-#         x = self.act(self.conv2(x))
-#         x = self.maxpool(x)
-#         x = torch.flatten(x, 1)
-#         x = self.act(self.fc1(x))
-#         x = self.fc(x)
-#         return x
+# ====================================================================================================================
 
 class FedAvgCNN(nn.Module):
     def __init__(self, in_features=1, num_classes=10, dim=1024):
@@ -275,50 +243,6 @@ class CifarNet(nn.Module):
         x = F.log_softmax(x, dim=1)
         return x
 
-# ====================================================================================================================
-
-# cfg = {
-#     'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-#     'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-#     'VGGbatch_size': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-#     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-# }
-
-# class VGG(nn.Module):
-#     def __init__(self, vgg_name):
-#         super(VGG, self).__init__()
-#         self.features = self._make_layers(cfg[vgg_name])
-#         self.classifier = nn.Sequential(
-#             nn.Linear(512, 512),
-#             nn.ReLU(True),
-#             nn.Linear(512, 512),
-#             nn.ReLU(True),
-#             nn.Linear(512, 10)
-#         )
-
-#     def forward(self, x):
-#         out = self.features(x)
-#         out = out.view(out.size(0), -1)
-#         out = self.classifier(out)
-#         output = F.log_softmax(out, dim=1)
-#         return output
-
-#     def _make_layers(self, cfg):
-#         layers = []
-#         in_channels = 3
-#         for x in cfg:
-#             if x == 'M':
-#                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-#             else:
-#                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-#                            nn.BatchNorm2d(x),
-#                            nn.ReLU(inplace=True)]
-#                 in_channels = x
-#         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-#         return nn.Sequential(*layers)
-
-# ====================================================================================================================
-
 def init_weights(m):
     classname = m.__class__.__name__
     if classname.find('Conv2d') != -1 or classname.find('ConvTranspose2d') != -1:
@@ -362,35 +286,6 @@ class LeNet(nn.Module):
         x = self.fc(x)
         x = F.log_softmax(x, dim=1)
         return x
-
-# ====================================================================================================================
-
-# class CNNCifar(nn.Module):
-#     def __init__(self, num_classes=10):
-#         super(CNNCifar, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, batch_size, 5)
-#         self.fc1 = nn.Linear(batch_size * 5 * 5, 120)
-#         self.fc2 = nn.Linear(120, 100)
-#         self.fc3 = nn.Linear(100, num_classes)
-
-#         # self.weight_keys = [['fc1.weight', 'fc1.bias'],
-#         #                     ['fc2.weight', 'fc2.bias'],
-#         #                     ['fc3.weight', 'fc3.bias'],
-#         #                     ['conv2.weight', 'conv2.bias'],
-#         #                     ['conv1.weight', 'conv1.bias'],
-#         #                     ]
-                            
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, batch_size * 5 * 5)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         x = F.log_softmax(x, dim=1)
-#         return x
 
 # ====================================================================================================================
 
@@ -511,15 +406,3 @@ class TextCNN(nn.Module):
         out = F.log_softmax(out, dim=1)
 
         return out
-
-# ====================================================================================================================
-
-
-# class linear(Function):
-#   @staticmethod
-#   def forward(ctx, input):
-#     return input
-  
-#   @staticmethod
-#   def backward(ctx, grad_output):
-#     return grad_output
