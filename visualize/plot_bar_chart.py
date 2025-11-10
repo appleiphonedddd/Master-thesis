f19ce72797e7d2dbb2632b743a2a5240b4a383b4
Egor <611221201@gms.ndhu.edu.tw>
2025-11-10 17:17:25 +0800

    2025-11-10

--- visualize/plot_bar_chart.py
+++ visualize/plot_bar_chart.py
@@ -43,11 +43,11 @@ def plot_accuracy(methods, datasets, accuracy):
 methods = ['FedAvg', 'FedDodm', 'FedAS', 'DCPFL', 'FedFIP']
 datasets = ['MNIST','FMNIST', 'CIFAR10', 'CIFAR100', 'Tiny-ImageNet']
 accuracy = [
-    [98.18, 85.80, 56.68, 32.48, 19.66],    # FedAvg
-    [99.65, 97.57, 89.90, 56.52, 37.66],    # FedDodm
-    [99.68, 97.74, 91.55, 59.78, 40.09],    # FedAS
-    [99.25, 97.36, 89.55, 46.88, 27.00],    # DCPFL
-    [99.68, np.NaN, 91.56, 61.84, 42.84]   # FedFIP
+    [98.18, 85.80, 56.68, 32.48, 19.66],  # FedAvg
+    [99.65, 97.57, 89.90, 56.52, 37.66],  # FedDodm
+    [99.68, 97.74, 91.55, 59.78, 40.09],  # FedAS
+    [99.25, 97.36, 89.55, 46.88, 27.00],  # DCPFL
+    [99.68, 97.82, 91.56, 61.84, 42.84]   # FedFIP
 ]
 
 plot_accuracy(methods, datasets, accuracy)
