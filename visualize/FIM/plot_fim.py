ca0b73d4f827d003cb2b7189ba8bd899b9326de7
Egor <zozo5120@outlook.com>
2025-11-19 20:54:29 +0800

    2025-11-19

--- visualize/FIM/plot_fim.py
+++ visualize/FIM/plot_fim.py
@@ -46,12 +46,12 @@ def plot_all_clients(csv_path: str, rolling_window=None):
 
     plt.xlabel("Communication Round", fontsize=14)
     plt.ylabel("FIM Trace", fontsize=14)
-    plt.title("FIM Trace per Client on MNIST", fontsize=16)
+    plt.title("FIM Trace per Client on Cifar10", fontsize=16)
     plt.grid(True, linewidth=0.3, alpha=0.6)
     plt.legend(fontsize=9, ncol=3)
     plt.tight_layout()
-    plt.savefig("MNIST.png", dpi=300)
+    plt.savefig("Cifar10.png", dpi=300)
     print("Finished plotting FIM traces. Saved to fim_trace_per_client.png")
 
 if __name__ == "__main__":
-    plot_all_clients("MNIST.csv", rolling_window=None)
+    plot_all_clients("Cifar10.csv", rolling_window=None)
