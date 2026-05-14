43ccd9a771e30cffdf7f05377ad5223de6541d58
Egor <611221201@gms.ndhu.edu.tw>
2026-05-14 13:30:20 +0800

    New

--- system/Cifar10.sh
+++ system/Cifar10.sh
@@ -1,13 +1,11 @@
 python main.py -data Cifar10 -m CNN -algo FedAvg -gr 300 -did 0
 
-python main.py -data Cifar10 -m CNN -algo FedPer -gr 300 -did 0
-
 python main.py -data Cifar10 -m CNN -algo FedBABU -gr 300 -did 0
 
 python main.py -data Cifar10 -m CNN -algo FedProto -gr 300 -did 0
 
 python main.py -data Cifar10 -m CNN -algo FedALA -gr 300 -did 0
 
-python main.py -data Cifar10 -m CNN -algo FedPAC -gr 300 -did 0
-
 python main.py -data Cifar10 -m CNN -algo FedAS -gr 300 -did 0
+
+python main.py -data Cifar10 -m CNN -algo FedCLAM -gr 300 -did 0
--- system/Tiny.sh
+++ system/Tiny.sh
@@ -2,12 +2,10 @@ python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedAvg -gr 300 -did 0 -l
 
 python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedBABU -gr 300 -did 0 -lr 0.05 -ld True
 
-python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedRep -gr 300 -did 0 -lr 0.05 -ld True
-
 python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedProto -gr 300 -did 0 -lr 0.05 -ld True
 
 python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedALA -gr 300 -did 0 -lr 0.05 -ld True
 
-python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedPAC -gr 300 -did 0 -lr 0.05 -ld True
-
 python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedAS -gr 300 -did 0 -lr 0.05 -ld True
+
+python main.py -data TinyImagenet -ncl 200 -m CNN -algo FedCALM -gr 300 -did 0 -lr 0.05 -ld True
